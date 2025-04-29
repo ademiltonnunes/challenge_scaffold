748fe93ba8e692d3723b7f856a881780fb1a5b57
ademiltonnunes <ademilton.marcelo1@gmail.com>
2025-04-28 22:37:48 -0700

    fixing database connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
@@ -10,9 +11,4 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_db():
-    db = Session()
-    try:
-        yield db
-    finally:
-        db.close()
+Base = declarative_base()
